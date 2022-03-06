3a04a7d1098c6ac14b0ad31d815b0f6db0c5bb8d
Celina Takeda <takedacarina@gmail.com>
2022-03-06 10:29:43 -0300

    commit

--- finder.js
+++ finder.js
@@ -52,18 +52,18 @@ function getAllData() {
 
     USER_INFO.innerHTML = `
     <div class="user-info">
-    <div class="user-avatar">
-    <h2 class="user-name">${USER.name == null ? USER.name = "No username" : USER.name}</h2>
-    <h3 class="user-login">${USER.login}</h3>
-    <img class="avatar_img" src="${USER.avatar_url}" alt="${USER.login}">
-    <p class="bio">${USER.bio == null ? (USER.bio = "No bio") : USER.bio}</p>
-    </div>
-    <div class="user-stats">
-    <h2>Info</h2>
-    <p>Repos: ${USER.public_repos}</p>
-    <p>Followers: ${USER.followers}</p>
-    <p>Following: ${USER.following}</p>
-    </div>
+        <div class="user-avatar">
+            <h2 class="user-name">${USER.name == null ? USER.name = "No username" : USER.name}</h2>
+            <h3 class="user-login">${USER.login}</h3>
+            <img class="avatar_img" src="${USER.avatar_url}" alt="${USER.login}">
+            <p class="bio">${USER.bio == null ? (USER.bio = "No bio") : USER.bio}</p>
+        </div>
+        <div class="user-stats">
+            <h2>Info</h2>
+            <p>Repos: ${USER.public_repos}</p>
+            <p>Followers: ${USER.followers}</p>
+            <p>Following: ${USER.following}</p>
+        </div>
     </div>
     ${USER.repos.map((repo) => `<div class="repo">
     <h3 class="repo-name">${repo.name}</h3>
